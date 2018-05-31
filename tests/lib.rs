ab5e451deec17361718c85baa5fd5511ed47c460
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-06-01 08:28:00 +0900

    testの修正

--- tests/lib.rs
+++ tests/lib.rs
@@ -2,10 +2,26 @@ extern crate reversi;
 use reversi::reversi_board::ReversiBoard;
 
 #[test]
-fn show_test(){
-    let hoge=ReversiBoard::new();
-    assert_eq!(hoge.to_string(),"eeeeeeee\neeeeeeee\neeeeeeee\neee○×eee\neee×○eee\neeeeeeee\neeeeeeee\neeeeeeee\n");
+fn show_test() {
+    let hoge = ReversiBoard::new();
+    assert_eq!(hoge.to_string(),
+"  | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 |\n\
+――――――――――――――――――――――――――――――――――\n\
+0 |   |   |   |   |   |   |   |   | \n\
+――――――――――――――――――――――――――――――――――\n\
+1 |   |   |   |   |   |   |   |   | \n\
+――――――――――――――――――――――――――――――――――\n\
+2 |   |   |   |   |   |   |   |   | \n\
+――――――――――――――――――――――――――――――――――\n\
+3 |   |   |   | ○ | × |   |   |   | \n\
+――――――――――――――――――――――――――――――――――\n\
+4 |   |   |   | × | ○ |   |   |   | \n\
+――――――――――――――――――――――――――――――――――\n\
+5 |   |   |   |   |   |   |   |   | \n\
+――――――――――――――――――――――――――――――――――\n\
+6 |   |   |   |   |   |   |   |   | \n\
+――――――――――――――――――――――――――――――――――\n\
+7 |   |   |   |   |   |   |   |   | \n");
 }
-
 
 
